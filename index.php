9e47cc8718c5fb811ca10bfd87ea067def94b9e9
Geferson Moreno <leonaikervan2001@gmail.com>
2023-01-18 18:26:34 -0400

    creando el app

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
-session_start();
-include_once('config.php');
+// session_start();
+// include_once('config.php');
 
-if ($_SESSION["id"]) {
-  include_once('./module/html.php');
+// if ($_SESSION["id"]) {
+//   include_once('./module/html.php');
 
-  $content = function () {
-    include_once('./module/indexHtml.php');
-    include_once('./module/header.php');
-    include_once('./module/carrousel.php');
-  };
-  template($content, 'Main page');
-} else {
-  comeBack("login.php");
-}
+//   $content = function () {
+//     include_once('./module/indexHtml.php');
+//     include_once('./module/header.php');
+//     include_once('./module/carrousel.php');
+//   };
+//   template($content, 'Main page');
+// } else {
+//   comeBack("login.php");
+// }
+
+require_once 'libs/app.php';
+
+$app = new app();
+
 ?>
---
+++ libs/app.php
@@ -0,0 +1,11 @@
+<?php
+
+class app {
+  function __construct(){
+    echo '<p>app</p>';
+    
+    
+  }
+}
+
+?>
