24d4b3a4cac2bf1aff83599e664f227517eb05d6
Geferson Moreno <leonaikervan2001@gmail.com>
2023-02-01 14:49:45 -0400

    regresando a la version estable

--- index.php
+++ index.php
@@ -5,13 +5,13 @@ include_once('config.php');
 if ($_SESSION["id"]) {
   include_once('./module/html.php');
 
-  $content = function () {
+  $content = function(){
     include_once('./module/indexHtml.php');
     include_once('./module/header.php');
     include_once('./module/carrousel.php');
   };
   template($content, 'Main page');
 } else {
-  comeBack("login.php")
+  comeBack("login.php");
 }
 ?>
