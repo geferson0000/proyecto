bd56dd9e181484970f14ff84ca09151bb447e7db
Geferson Moreno <leonaikervan2001@gmail.com>
2023-04-14 14:43:16 -0400

    cleaning

--- signup.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('./module/html.php');
-
-$content = function(){ // empaquetando el contenido
-  include_once('./module/signupHtml.php');
-};
-
-template($content, 'Sign up'); // utilizando el HTML para mostrar lo empaquetado
-?>
